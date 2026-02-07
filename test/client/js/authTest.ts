209cd9d16fc21cf84d1551d664c5ea7ea88a8541
Simon Hartcher <simon@simonhartcher.com>
2026-02-07 23:58:06 +1100

    test(auth): add tests for Auth.signout with logoutUrl parameter

--- test/client/js/authTest.ts
+++ test/client/js/authTest.ts
@@ -8,15 +8,18 @@ describe("Auth", function () {
 	describe(".signout", function () {
 		let localStorageClearStub: sinon.SinonStub<[], void>;
 		let locationReloadStub: sinon.SinonStub<[], void>;
+		let locationReplaceStub: sinon.SinonStub<[string], void>;
 
 		beforeEach(function () {
 			localStorageClearStub = sinon.stub(localStorage, "clear");
 			locationReloadStub = sinon.stub(location, "reload");
+			locationReplaceStub = sinon.stub(location, "replace");
 		});
 
 		afterEach(function () {
 			localStorageClearStub.restore();
 			locationReloadStub.restore();
+			locationReplaceStub.restore();
 		});
 
 		it("should empty the local storage", function () {
@@ -25,10 +28,26 @@ describe("Auth", function () {
 			expect(localStorage.clear.calledOnce).to.be.true;
 		});
 
-		it("should reload the page", function () {
+		it("should reload the page when no logoutUrl provided", function () {
 			Auth.signout();
 			// @ts-expect-error ts-migrate(2339) FIXME: Property 'calledOnce' does not exist on type '{ ()... Remove this comment to see the full error message
 			expect(location.reload.calledOnce).to.be.true;
+			expect(locationReplaceStub.called).to.be.false;
+		});
+
+		it("should redirect to logoutUrl when provided", function () {
+			const logoutUrl = "https://auth.example.com/logout";
+			Auth.signout({logoutUrl});
+			expect(locationReplaceStub.calledOnce).to.be.true;
+			expect(locationReplaceStub.calledWith(logoutUrl)).to.be.true;
+			expect(locationReloadStub.called).to.be.false;
+		});
+
+		it("should clear storage before redirecting to logoutUrl", function () {
+			Auth.signout({logoutUrl: "https://auth.example.com/logout"});
+			// @ts-expect-error ts-migrate(2339) FIXME: Property 'calledOnce' does not exist on type '() =... Remove this comment to see the full error message
+			expect(localStorage.clear.calledOnce).to.be.true;
+			expect(localStorageClearStub.calledBefore(locationReplaceStub)).to.be.true;
 		});
 	});
 });
